ecc1ba850511ad8aa624d239d708e403ab702ef1
1AnthonyGreene <97619586+1AnthonyGreene@users.noreply.github.com>
2023-09-12 18:03:54 -0400

    v2 tips calculator.py

--- tips calculator.py
+++ tips calculator.py
@@ -1,37 +1,53 @@
+#!usr/bin/env python3
+
 from math import floor
-from os import sep
-from tkinter import N
+from math import ceil
 
 credit = float(input("Enter credit card tips amount: "))
 cash = float(input("Enter cash tips amount: "))
-diner = float(input ("Enter diner tips amount: "))
+
+
+diner = 0
+lst = []
+x = input("\nEnter diner tips, seperating each with a space: ")
+x = x.split(" ")
+
+
+for i in range(len(x)):
+    diner += (float(x[i]))
+
 print ("\n\n")
 
 
 total = floor(credit + cash)
-print ("Your total tips is: $" +str(total))
+print ("\n 3. Your total tips is: $" +str(total))
 
 bar = floor(total * .1)
-print("The tips for bar is: $" + str(bar))
+print("\n 4. The tips for bar is: $" + str(bar))
 
 total -= bar
 
-print ("The amount after bar is: $" + str(total))
+print ("\n 5. Total after bar: $" + str(total))
 
 urdiner = round(diner * .6)
 othrdiner= int(diner - urdiner)
 
-print ("Your diner tips are: $" + str(urdiner))
-print ("The tips for sushibar are: $" + str(othrdiner))
+print ("\n 6. Total diner: $" + str(ceil(diner)))
+
+print ("\n 7. Tips for sushi chef: $" + str(othrdiner))
+print ("\n 8. Your sushi/diner tips: $" + str(urdiner))
+
 
 teppan = round(total - diner)
-print ("Your tips after diner is: $" + str(teppan))
+print ("\n 9. Teppan tips left: $" + str(teppan))
 urteppan = round(teppan * .55)
 othrteppan = int(teppan - urteppan)
 
-print ("Your teppan tips are: $" + str(urteppan))
-print ("The tips for the chefs are: $" + str(othrteppan))
+print ("\n 10. Your teppan tips: $" + str(urteppan))
+print ("\n 11. Tips for the chefs: $" + str(othrteppan))
 
-print("The amount of tips you give to bar is: $" + str(bar + othrdiner))
+print("\n 12.Tips you give to bar: $" + str(bar + othrdiner))
 
-print ("The tips you take home is: $" + str((urdiner + urteppan)))
+print ("\n 13. The tips you take home: $" + str((urdiner + urteppan)))
+
+print("\n2% for Anthony: $" + str(ceil((urdiner + urteppan) * .02)))
